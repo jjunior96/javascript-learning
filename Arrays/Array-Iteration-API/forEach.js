5aaead8f0b09017986ff46dd8ef83baee123ce92
Junior <jjalves96@gmail.com>
2020-08-28 16:02:53 -0300

    add forEach

---
+++ Arrays/Array-Iteration-API/forEach.js
@@ -0,0 +1,32 @@
+/**
+ * Executa a função passada por parâmetro
+ * para cada elemento.
+**/
+
+const alunos = ['José', 'Maria', 'João'];
+alunos.forEach(function (aluno) {
+  console.log(aluno);
+});
+
+/** 
+ * Ou, com Array Function
+**/
+
+alunos.forEach( (aluno) => console.log(aluno) );
+
+/** 
+ * Obs.: No momento que fazemos "alunos.forEach",
+ * os elementos já são processados.
+**/
+
+alunos.forEach((aluno) => {
+  alunos.push('Marcia');
+  console.log(aluno);
+});
+/**
+ * José
+ * Maria
+ * João
+ * 
+ * Marcia não foi exibida!
+**/
